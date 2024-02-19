36314c4416c65673ab8031ec57bd4c1152835607
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-19 13:05:01 -0400

    Saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,14 @@
 import Saludar from "./Saludar";
+import nombre from "./nombre";
 
-//const nombre_input = document.querySelector("#nombre");
+const nombre_input = document.querySelector("#nombre");
 const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p> Hola" +Saludar + "</p>";
+  const nombreValue = nombre_input.value;
+
+  div.innerHTML = "<p>Hola " + Saludar(nombreValue) + "</p>";
 });
