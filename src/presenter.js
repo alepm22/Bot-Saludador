fe6bfcebb348b1972d721a47f4c63133a8aa0128
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-19 14:20:42 -0400

    Saludar por nombre + genero + edad + hr + idioma

--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,12 @@ import Saludar from "./Saludar";
 import nombre from "./nombre";
 import genero from "./genero";
 import edad from "./edad";
+import idioma from "./idioma";
 
 const nombre_input = document.querySelector("#nombre");
 const genero_input = document.querySelector("#genero");
 const edad_input = document.querySelector("#edad");
+const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -15,31 +17,31 @@ form.addEventListener("submit", (event) => {
   const nombreValue = nombre_input.value;
   const generoValue = genero_input.value;
   const edadValue = parseInt(edad_input.value); 
+  const idiomaValue = idioma_input.value;
 
-  // Documento que nos proporciono
+  let saludoHora;
+
+  // Documento proporcionado
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
 
-  let saludoHora;
-  if (horaActual >= 6 && horaActual < 12) {
-    saludoHora = "Buenos días";
-  } else if (horaActual >= 12 && horaActual < 18) {
-    saludoHora = "Buenas tardes";
-  } else {
-    saludoHora = "Buenas noches";
+  if (idiomaValue === "espanol") {
+    saludoHora = (horaActual >= 6 && horaActual < 12) ? "Buenos días" :
+                 (horaActual >= 12 && horaActual < 18) ? "Buenas tardes" :
+                 "Buenas noches";
+  } else if (idiomaValue === "ingles") {
+    saludoHora = (horaActual >= 6 && horaActual < 12) ? "Good Morning" :
+                 (horaActual >= 12 && horaActual < 18) ? "Good Afternoon" :
+                 "Good Night";
   }
 
-  let mensajeSaludo = saludoHora + ", ";
+  let mensajeSaludo = `${saludoHora}, `;
 
-  if (generoValue === "f" && edadValue >= 30) {
-    mensajeSaludo += "Sra. " + Saludar(nombreValue, generoValue);
-  } else if (generoValue === "f" && edadValue < 30) {
-    mensajeSaludo += "amiga " + Saludar(nombreValue, generoValue);
-  } else if (generoValue === "m" && edadValue >= 30) {
-    mensajeSaludo += "Sr. " + Saludar(nombreValue, generoValue);
-  } else {
-    mensajeSaludo += "amigo " + Saludar(nombreValue, generoValue);
-  }
-
-  div.innerHTML = "<p>" + mensajeSaludo + "</p>";
+  let titulo = (idiomaValue === "espanol") ? (edadValue >= 30 ? (generoValue === "f" ? "Sra." : "Sr.") : (generoValue === "f" ? "amiga" : "amigo")) :
+                (idiomaValue === "ingles" ? (edadValue >= 30 ? (generoValue === "f" ? "Mrs." : "Mr.") : "friend") : "");
+  
+  mensajeSaludo += `${titulo} ${Saludar(nombreValue, generoValue)}`;
+  
+  div.innerHTML = `<p>${mensajeSaludo}</p>`;
+  
 });
