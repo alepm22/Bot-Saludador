455878928f697154569842fb245dea0b131b056b
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-19 12:50:28 -0400

    Saludar con un HOLA

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
+import Saludar from "./Saludar";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+//const nombre_input = document.querySelector("#nombre");
+const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p> Hola" +Saludar + "</p>";
 });
