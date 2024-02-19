de3cecd4ca2bb83a2b8173f9118b6306221a668e
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-19 13:25:41 -0400

    Saludar por nombre + genero + edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
 import Saludar from "./Saludar";
 import nombre from "./nombre";
 import genero from "./genero";
+import edad from "./edad";
 
 const nombre_input = document.querySelector("#nombre");
 const genero_input = document.querySelector("#genero");
+const edad_input = document.querySelector("#edad");
 const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,10 +14,15 @@ form.addEventListener("submit", (event) => {
 
   const nombreValue = nombre_input.value;
   const generoValue = genero_input.value;
- if(genero == "f")
- {
-  div.innerHTML = "<p>Hola Sra. " + Saludar(nombreValue, generoValue) + "</p>";
- }else
- div.innerHTML = "<p>Hola Sr. " + Saludar(nombreValue, generoValue) + "</p>";
+  const edadValue = parseInt(edad_input.value); // Aseguramos que la edad sea un número
 
+  if (generoValue === "f" && edadValue >= 30) {
+    div.innerHTML = "<p>Hola Sra. " + Saludar(nombreValue, generoValue) + "</p>";
+  } else if (generoValue === "f" && edadValue < 30) {
+    div.innerHTML = "<p>Hola amiga " + Saludar(nombreValue, generoValue) + "</p>";
+  } else if (generoValue === "m" && edadValue >= 30) {
+    div.innerHTML = "<p>Hola Sr. " + Saludar(nombreValue, generoValue) + "</p>";
+  } else {
+    div.innerHTML = "<p>Hola amigo " + Saludar(nombreValue, generoValue) + "</p>";
+  }
 });
