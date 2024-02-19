a1b1d84ef60149d062b83b443c5652b01127035e
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-19 13:16:34 -0400

    Saludar por nombre + genero Sr. / Sra.

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,9 @@
 import Saludar from "./Saludar";
 import nombre from "./nombre";
+import genero from "./genero";
 
 const nombre_input = document.querySelector("#nombre");
+const genero_input = document.querySelector("#genero");
 const form = document.querySelector("#Saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,6 +11,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombreValue = nombre_input.value;
+  const generoValue = genero_input.value;
+ if(genero == "f")
+ {
+  div.innerHTML = "<p>Hola Sra. " + Saludar(nombreValue, generoValue) + "</p>";
+ }else
+ div.innerHTML = "<p>Hola Sr. " + Saludar(nombreValue, generoValue) + "</p>";
 
-  div.innerHTML = "<p>Hola " + Saludar(nombreValue) + "</p>";
 });
